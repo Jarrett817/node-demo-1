3f97a724e011f14120000da1416a00589c7ca882
Jarrett <1206905682@qq.com>
2020-07-30 14:04:37 +0800

    修改response

--- server.js
+++ server.js
@@ -31,13 +31,13 @@ var server = http.createServer(function (request, response) {
         response.write(`
         <!DOCTYPE html>
         <head>
-        <link rel="stylesheet" href="/x">
+        <link rel="stylesheet" href="/style.css">
         </head>
         <body>
         <h1>标题</h1>
         </body>`)
         response.end()
-    } else if (path === '/x') {
+    } else if (path === '/style.css') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
         response.write(`body{color: red;}`)
